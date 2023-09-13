8f30d2c0526e5b3bbe8cbdb1f84c4b4282082337
ZhiyuanYan <zyan760@connect.hkust-gz.edu.cn>
2023-09-13 11:21:34 +0800

    after_merging

--- utils/filter.cpp
+++ utils/filter.cpp
@@ -36,23 +36,27 @@ AntFilter::AntFilter(const std::string filename, const std::string& filter, Tran
 
 
 bool AntFilter::operator()(const std::string name_check, std::string val_check,const int idx0,const int idx1) const{
-        for(const auto & coi: COI_to_consider_){
-          const auto & var_name = coi.first;
-          auto pos = ts_.named_terms().find(var_name);
-          assert(pos != ts_.named_terms().end());
-          if(name_check!=var_name)
-            continue;
-          auto pos_1 = COI_value.find(var_name);
-          assert(pos_1 != COI_value.end());
-          auto value = pos_1->second;
-          assert(value.substr(0,2)=="#b");
-          value = value.substr(2);
-          assert(value.length()==val_check.length());
-          std::reverse(value.begin(), value.end());
-          auto extract_val = value.substr(idx1,idx0-idx1+1);
-          std::reverse(val_check.begin(), val_check.end());
-          auto extract_val_check = val_check.substr(idx1,idx0-idx1+1);
-          return (extract_val_check==extract_val);
-      }
+      if(COI_to_consider_.empty())
+        return false;
+
+      for(const auto & coi: COI_to_consider_){
+        const auto & var_name = coi.first;
+        auto pos = ts_.named_terms().find(var_name);
+        assert(pos != ts_.named_terms().end());
+        if(name_check!=var_name)
+          continue;
+        auto pos_1 = COI_value.find(var_name);
+        assert(pos_1 != COI_value.end());
+        auto value = pos_1->second;
+        assert(value.substr(0,2)=="#b");
+        value = value.substr(2);
+        assert(value.length()==val_check.length());
+        std::reverse(value.begin(), value.end());
+        auto extract_val = value.substr(idx1,idx0-idx1+1);
+        std::reverse(val_check.begin(), val_check.end());
+        auto extract_val_check = val_check.substr(idx1,idx0-idx1+1);
+        return (extract_val_check==extract_val);
+    }
+
 }
 }
